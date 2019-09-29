e9ffc06a2e1f9fd09ad94ae102b84887248c6538
Benjamin Scholtz <contact@benjaminscholtz.com>
2019-09-29 19:17:22 +0200

    Addresses scraped

--- lib/example_web/controllers/hello_controller.ex
+++ lib/example_web/controllers/hello_controller.ex
@@ -4,8 +4,8 @@ defmodule ExampleWeb.HelloController do
   def index(conn, _params) do
     render(conn, "index.html")
   end
-  
-  def show(conn, %{"messenger" => messenger}) do
+
+  def scan(conn, %{"messenger" => messenger}) do
     render(conn, "show.html", messenger: messenger)
   end
 end
